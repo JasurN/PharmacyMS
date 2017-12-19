54b8b6052ad65a746e688db19ad6970b5251eef1
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2017-12-20 02:44:43 +0500

    1. Updated database .sql

--- db/PharmacyDB.sql
+++ db/PharmacyDB.sql
@@ -1,21 +1,21 @@
 CREATE TABLE authorization(
-  id VARCHAR (10) not NULL,
-  password VARCHAR(20) not NULL,
+  id VARCHAR (10) NOT NULL,
+  password VARCHAR(20) NOT NULL,
   type NUMERIC(1) check (type in (0, 1, 2)),
   PRIMARY KEY (id)
 );
 CREATE TABLE company (
-  comp_id    VARCHAR(10) not NULL,
-  comp_name VARCHAR(20) not NULL,
+  id    VARCHAR(10) NOT NULL,
+  comp_name VARCHAR(20) NOT NULL,
   comp_address  VARCHAR(40),
   comp_contact VARCHAR(20),
-  PRIMARY KEY (comp_id),
+  PRIMARY KEY (id),
   FOREIGN KEY (comp_id) REFERENCES authorization(id)
 );
 
 CREATE TABLE drugstore (
-  store_id    VARCHAR(10) not NULL,
-  store_name VARCHAR(20) not NULL,
+  id    VARCHAR(10) NOT NULL,
+  store_name VARCHAR(20) NOT NULL,
   store_address  VARCHAR(40),
   store_contact VARCHAR(20),
   PRIMARY KEY (store_id),
@@ -23,20 +23,20 @@ CREATE TABLE drugstore (
 );
 
 CREATE TABLE medicine (
-  med_id    VARCHAR(10) not NULL,
-  med_name VARCHAR(20) not NULL,
+  med_id    VARCHAR(10) NOT NULL,
+  med_name VARCHAR(20) NOT NULL,
   med_description  VARCHAR(200),
-  med_price NUMERIC(12, 2) not NULL,
-  comp_id   VARCHAR(10) not NULL,
+  med_price NUMERIC(12, 2) NOT NULL,
+  comp_id   VARCHAR(10) NOT NULL,
   PRIMARY KEY (med_id),
   FOREIGN KEY (comp_id) REFERENCES company(comp_id)
   ON DELETE CASCADE
 );
 
 CREATE TABLE inventory(
-store_id VARCHAR(10) not null,
-med_id VARCHAR(10) not null,
-med_name VARCHAR(20) not null,
+store_id VARCHAR(10) NOT NULL,
+med_id VARCHAR(10) NOT NULL,
+med_name VARCHAR(20) NOT NULL,
 med_quantity NUMERIC(4),
 PRIMARY KEY (store_id, med_id),
 FOREIGN KEY (store_id) REFERENCES drugstore(store_id)
@@ -47,13 +47,13 @@ on DELETE CASCADE
 
 CREATE TABLE journal
  (
-  trans_id    INTEGER(11) not NULL AUTO_INCREMENT,
+  trans_id    INTEGER(11) NOT NULL AUTO_INCREMENT,
   trans_date timestamp,
-  comp_id   VARCHAR(10) not NULL,
-  store_id VARCHAR(10) not NULL,
-  med_id VARCHAR(10) not NULL,
+  comp_id   VARCHAR(10) NOT NULL,
+  store_id VARCHAR(10) NOT NULL,
+  med_id VARCHAR(10) NOT NULL,
   quantity NUMERIC(4),
-  status NUMERIC(1) check (status in (0,1)),
+  status NUMERIC(1) check (status IN (0,1)),
   PRIMARY KEY (trans_id),
   FOREIGN KEY (comp_id) REFERENCES company(comp_id)
     ON DELETE CASCADE,
