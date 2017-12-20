dc6a6a40d4a50fff431e156d28075bb79f754487
Jasurbek Nabijonov <97jasur@gmail.com>
2017-12-20 23:11:37 +0500

    1. Refactor

--- src/client/client.c
+++ src/client/client.c
@@ -111,10 +111,10 @@ fromServer* produceMedicineByCompany(const char *drugName, const char *ID,
 
 }
 
-fromServer* orderNewMedecine(char* name, int quantity, char* id) {
+fromServer* orderNewMedecine(char* name, int quantity, char* storeId) {
     toServer *toServerObj = (toServer *) malloc(sizeof(toServer));
     toServerObj->type = PURCHASE;
-    strcpy(toServerObj->authorization.login, id);
+    strcpy(toServerObj->authorization.login, storeId);
 
     strcpy(toServerObj->purchase.name, name);
     toServerObj->purchase.quantity = quantity;
--- src/client/clientStarter.c
+++ src/client/clientStarter.c
@@ -1,9 +1,11 @@
 #include "client.h"
+#include "../parser/request_parser.h"
 
 int main() {
 
     //fromServer *fromServerObj = authorizationClient("1124", "12345");
-    fromServer *fromServerObj = orderNewMedecine("Parasetamol", 99, "1127");
+    fromServer *fromServerObj = orderNewMedecine("Trimol", 10, "1125");
+    printf("result %d", fromServerObj->purchase.success);
             free(fromServerObj);
     return 0;
 }
